bf460483b745f67823b9b53c6815852d36b9949a
jaron <jaronschulz@gmail.com>
2025-03-27 13:15:51 +0100

    refactor: remove redundant test-workflow.sh script

--- .github/workflows/utils/test-workflow.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Testing workflow: $1 with event: $2"
-echo "This is a placeholder. Replace with actual workflow testing logic."
-# Add actual act command here, e.g.:
-# act --workflows=.github/workflows/$1 --eventpath=.github/workflows/events/$2.json
